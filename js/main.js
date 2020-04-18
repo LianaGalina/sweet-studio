cecf1192b3a12966cce5a28bea37ebb85cd432d6
LianaGalina <61352944+LianaGalina@users.noreply.github.com>
2020-04-18 16:46:48 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-var mySwiper = new Swiper ('.swiper-container', {
-
-    direction: 'horizontal',
-    loop: true,
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-  });
-
 const menuButton = document.querySelector('.menu-button');
 const menu = document.querySelector('.menu');
+
 menuButton.addEventListener('click', function () {
   menu.classList.toggle('menu-active');
   menuButton.classList.toggle('menu-button-active');
+});
+
+$(document).ready(function(){
+    $(".menu").on("click","a", function (event) {
+        event.preventDefault();
+        const id  = $(this).attr('href'),
+              top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+const mySwiper = new Swiper ('.swiper-container', {
+
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
 });
