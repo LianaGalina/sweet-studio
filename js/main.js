87d0398b427c5f9e011ee90daefad8aad2956b24
LianaGalina <61352944+LianaGalina@users.noreply.github.com>
2020-04-25 18:37:37 +0500

    new 25/04

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-const menuButton=document.querySelector('.menu-button');const menu=document.querySelector('.menu');menuButton.addEventListener('click',function(){menu.classList.toggle('menu-active');menuButton.classList.toggle('menu-button-active')});$(document).ready(function(){$(".menu").on("click","a",function(event){event.preventDefault();const id=$(this).attr('href'),top=$(id).offset().top;$('body,html').animate({scrollTop:top},1500)})});const mySwiper=new Swiper('.swiper-container',{direction:'horizontal',loop:!0,navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev',},})
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+menuButton.addEventListener('click', function () {
+   menu.classList.toggle('menu-active');
+   menuButton.classList.toggle('menu-button-active')
+});
+$(document).ready(function () {
+   $(".menu").on("click", "a", function (event) {
+      event.preventDefault();
+      const id = $(this).attr('href'),
+         top = $(id).offset().top;
+      $('body,html').animate({
+         scrollTop: top
+      }, 1500)
+   })
+});
+const mySwiper = new Swiper('.swiper-container', {
+   direction: 'horizontal',
+   loop: !0,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+})
+
+const bonusText = document.querySelector('.bonus-text'),
+   bonusImg = document.querySelector('.bonus-img');
+
+bonusImg.addEventListener('click', () => {
+   bonusText.style.display = 'block';
+});
